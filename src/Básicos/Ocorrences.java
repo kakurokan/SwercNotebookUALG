f79514663afc6cddab732a441a65b5cd5124afce
kakurokan <leokakali7@gmail.com>
2025-11-21 10:39:05 +0000

    organizado

---
+++ src/Básicos/Ocorrences.java
@@ -0,0 +1,25 @@
+// Java program to count frequencies of elements
+// using HashMap.
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.*;
+
+class GFG {
+    public static void countFrequencies(ArrayList<String> list)
+    {
+        // hashmap to store the frequency of element
+        Map<String, Integer> hm = new HashMap<String, Integer>();
+
+        for (String i : list) {
+            Integer j = hm.get(i);
+            hm.put(i, (j == null) ? 1 : j + 1);
+        }
+
+        // displaying the occurrence of elements in the arraylist
+        for (Map.Entry<String, Integer> val : hm.entrySet()) {
+            System.out.println("Element " + val.getKey() + " "
+                    + "occurs"
+                    + ": " + val.getValue() + " times");
+        }
+    }
